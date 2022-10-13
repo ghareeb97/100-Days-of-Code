66ae2856b32ba29988bfccaa35f6ebc176cdf618
Mohamed Ghareeb <55588241+ghareeb97@users.noreply.github.com>
2022-10-13 23:43:14 +0200

    Day 17 edit

--- day17/main.py
+++ day17/main.py
@@ -1,9 +1,18 @@
-class Question:
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
-	def __init__(self, q_text, q_answer):
-		self.text = q_text
-		self.answer = q_answer
+question_bank = []
 
+for question in question_data:
+	t = question["text"]
+	a = question["answer"]
+	question_bank.append(Question(t, a))
 
-new_q = Question("sdf", "True")
-print(new_q.text)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_remaining_questions():
+	quiz.next_question()
+
+print("You've completed the all the questions")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
--- day17/question_model.py
+++ day17/question_model.py
@@ -4,6 +4,3 @@ class Question:
 		self.text = q_text
 		self.answer = q_answer
 
-
-new_q = Question("sdf", "True")
-print(new_q.text)
